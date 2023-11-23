e11aa3982edb869b339883593584896e03cc724a
paez-marie1019 <paez.marie1019@gmail.com>
2023-11-24 01:31:41 +0800

    Continuation Log In
    https://www.youtube.com/watch?v=_L46CaEI490&list=PL2OrQJM8zmZ2-O_rM2Ju9zYMbY8Ta-8I4&index=8
    
    16:14

--- src/main/java/com/wikangwiz/WikangWali/Service/UserDetailsServiceImpl.java
+++ src/main/java/com/wikangwiz/WikangWali/Service/UserDetailsServiceImpl.java
@@ -1,27 +1,26 @@
 package com.wikangwiz.WikangWali.Service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.wikangwiz.WikangWali.Entity.UserEntity;
-import com.wikangwiz.WikangWali.util.CustomPasswordEncoder;
+import com.wikangwiz.WikangWali.Repository.UserRepository;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
-	private CustomPasswordEncoder passwordEndoder;
+	private UserRepository urepo;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		UserEntity user = new UserEntity();
-		user.setUsername(username);
-		user.setPassword(passwordEndoder.getPasswordEncoder().encode("123"));
-		user.setUser_id(1);
-		return (UserDetails) user;
+		Optional<UserEntity> userOpt = urepo.findByUsername(username);
+		
+		return userOpt.orElseThrow(() -> new UsernameNotFoundException("Invalid credentials"));
 	}
 }
--- src/main/java/com/wikangwiz/WikangWali/filter/JwtFilter.java
+++ src/main/java/com/wikangwiz/WikangWali/filter/JwtFilter.java
@@ -1,5 +1,75 @@
 package com.wikangwiz.WikangWali.filter;
 
-public class JwtFilter {
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.wikangwiz.WikangWali.Repository.UserRepository;
+import com.wikangwiz.WikangWali.util.JwtUtil;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter{
+	
+	@Autowired
+	private UserRepository urepo;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        // Get authorization header and validate
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if ((!StringUtils.hasText(header)) || (StringUtils.hasText(header) && !header.startsWith("Bearer "))) {
+            chain.doFilter(request, response);
+            return;
+        }
+        
+        // Authorization -> ([Bearer], [fsdfwryhgriesujhfbyniwsehnfgisk])
+        final String token = header.split(" ")[1].trim();
+
+        // Get user identity and set it on the spring security context
+        UserDetails userDetails = urepo
+            .findByUsername(jwtUtil.getUsernameFromToken(token))
+            .orElse(null);
+        
+        // Get jwt token and validate
+        if (!jwtUtil.validateToken(token, userDetails)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken
+            authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null,
+                userDetails == null ?
+                    List.of() : userDetails.getAuthorities()
+            );
+
+        authentication.setDetails(
+            new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+
+        //Authentication happens, and the user is now valid
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        chain.doFilter(request, response);
+    }
 
 }
